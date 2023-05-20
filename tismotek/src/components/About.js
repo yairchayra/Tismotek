b769708841369b21047c6bd7c954ef21f62d4ba3
yairchayra <yairrahmany@gmail.com>
2023-05-20 12:30:12 +0300

    about change

--- tismotek/src/components/About.js
+++ tismotek/src/components/About.js
@@ -1,6 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../firebase'; // Import the db variable
 import './About.css';
-function About() {
-    return ( <p>About</p> );
-}
+
+const About = () => {
+  const [aboutData, setAboutData] = useState({ text: '', imageUrl: '' });
+
+  useEffect(() => {
+    const fetchAboutData = async () => {
+      try {
+        const aboutDocRef = doc(db, 'about', 'LRn1319mLuETvoJ3SCWv');
+        const docSnapshot = await getDoc(aboutDocRef);
+
+        if (docSnapshot.exists()) {
+          setAboutData(docSnapshot.data());
+        }
+      } catch (error) {
+        console.error('Error fetching about data:', error);
+      }
+    };
+
+    fetchAboutData();
+  }, []);
+  console.log(aboutData);
+  return (
+    <div className="about">
+        <div className="about__image">
+        <img src={aboutData.imageUrl} alt="About" />
+      </div>
+      <div className="about__text">{aboutData.text}</div>
+
+    </div>
+  );
+};
 
 export default About;
